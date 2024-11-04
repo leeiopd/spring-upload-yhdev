e76a69f0f6d2c259ece727281e93695fe2c4c263
migrationArc <leeiopd@gmail.com>
2024-11-04 10:01:27 +0900

    스프링 MVC 2편 - 파일 업로드 - 예제로 구현하는 파일 업로드, 다운로드

--- src/main/java/hello/upload/controller/SpringUploadController.java
+++ src/main/java/hello/upload/controller/SpringUploadController.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,12 +20,12 @@ public class SpringUploadController {
   @Value("${file.dir}")
   private String fileDir;
 
-  @RequestMapping("/upload")
+  @GetMapping("/upload")
   public String newFile() {
     return "upload-form";
   }
 
-  @RequestMapping("/upload")
+  @PostMapping("/upload")
   public String saveFile(@RequestParam String itemName,
       @RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
 
